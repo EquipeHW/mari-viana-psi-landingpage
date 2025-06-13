a8926e65b86774d694241c2ee4e2c3163b687113
João Oliveira <215558335+EquipeHW@users.noreply.github.com>
2025-06-13 13:29:51 -0300

    imagem

--- app/page.tsx
+++ app/page.tsx
@@ -469,7 +469,7 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-12">
             <div className="space-y-6">
               <div className="flex items-center gap-2">
-                <div className="relative h-20 w-48">
+                <div className="relative h-20 w-full">
                   <Image
                     src="logobrancamari.png"
                     alt="Mariana Viana Psicologia"
